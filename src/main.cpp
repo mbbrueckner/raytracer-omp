df351585b234ed6fd8eaceb788fd77e43f6c0cfc
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-05 12:25:46 +0200

    feat: add main

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "serial_renderer.hpp"
+#include "utils/vec3.hpp"
+
+using raytrace::utils::Vec3;
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    throw std::invalid_argument(
+        std::format("Usage: {} <stl_file> [width] [height]", argv[0]));
+  }
+
+  const std::string stl_file = argv[1];
+
+  int width = 600;
+  int height = 600;
+  if (argc > 2) width = std::stoi(argv[2]);
+  if (argc > 3) height = std::stoi(argv[3]);
+
+  const Vec3 cam_pos(20, -20, 10);
+  const Vec3 cam_lookat(0, 0, 3);
+  const Vec3 cam_up(0, 0, 1);
+  const Vec3 light_source(20, -20, 5);
+
+  raytrace::serial::render(
+      width, height, stl_file, cam_pos, cam_lookat, cam_up, light_source);
+  return 0;
+}
