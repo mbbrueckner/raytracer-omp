ad31b4f498c8a91d0f3a5e3d437d5b1a446b32a1
mbbrueckner <mikabenbrueckner@icloud.com>
2026-06-05 11:57:02 +0200

    feat: add io

---
+++ include/utils/io.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <array>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "triangle.hpp"
+#include "vec3.hpp"
+
+namespace raytrace {
+namespace utils {
+namespace io {
+
+inline std::vector<Triangle> load_stl(const std::string& path,
+                                      const Vec3& color) {
+  std::vector<Triangle> tris;
+  std::ifstream file(path);
+
+  if (!file) {
+    throw std::runtime_error("STL file not found: " + path);
+  }
+
+  std::vector<Vec3> verts;
+  std::string line;
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string keyword;
+    double x, y, z;
+    if ((iss >> keyword >> x >> y >> z) && keyword == "vertex") {
+      verts.emplace_back(x, y, z);
+      if (verts.size() == 3) {
+        tris.push_back(Triangle{verts[0], verts[1], verts[2], color});
+        verts.clear();
+      }
+    }
+  }
+  return tris;
+}
+
+inline void write_ppm(const std::string& fname,
+                      int width,
+                      int height,
+                      const std::vector<std::array<int, 3>>& pixels) {
+  std::ofstream file(fname);
+  file << "P3\n" << width << " " << height << "\n255\n";
+  for (const std::array<int, 3>& px : pixels) {
+    file << px[0] << " " << px[1] << " " << px[2] << "\n";
+  }
+}
+
+}  // namespace io
+}  // namespace utils
+}  // namespace raytrace
